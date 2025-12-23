df6bdd11060637ed8a7fac4bc20d19abdce4a5c7
pierreguillaumelaurin <pierreguillaumelaurin@gmail.com>
2025-12-22 20:38:48 -0500

    Day 5 part 2

--- solutions/day05.rb
+++ solutions/day05.rb
@@ -12,8 +12,27 @@ def part1(input)
   end.length
 end
 
+def without_overlaps(ranges)
+  sorted_ranges = ranges.sort_by(&:first)
+
+  sorted_ranges.each_with_object([]) do |range, merged|
+    if merged.empty? || range.first > merged.last.last
+      merged << range
+    else
+      max = [merged.last.last, range.last].max
+      merged[-1] = [merged.last.first, max]
+    end
+  end
+end
+
 def part2(input)
-  # TODO
+  unparsed_ranges, = input.split("\n\n")
+
+  ranges = unparsed_ranges
+           .lines
+           .flat_map { _1.lines.map { |line| line.chomp.split('-').map(&:to_i) } }
+
+  without_overlaps(ranges).map { |(floor, ceil)| (floor..ceil) }.sum(&:count)
 end
 
 real_input = File.read('day05-input.txt')
@@ -21,3 +40,5 @@ real_input = File.read('day05-input.txt')
 puts "Part 1: #{part1(real_input)}"
 # answer 577
 puts "Part 2: #{part2(real_input)}"
+# 432907450074353 too high
+# 395635883201578 too high
