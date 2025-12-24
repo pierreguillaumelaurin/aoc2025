7f681f6ab40ac36dffa12caabc82d2847d8e4058
pierreguillaumelaurin <pierreguillaumelaurin@gmail.com>
2025-12-24 16:28:45 -0500

    Day 6 part 2

--- solutions/day06.rb
+++ solutions/day06.rb
@@ -12,11 +12,43 @@ def part1(input)
 end
 
 def part2(input)
-  # TODO
+  columns = input
+            .lines
+            .map(&:chomp)
+            .map { _1.split('') }
+            .transpose
+
+  numbers = columns.each_with_object([]) do |group, arr|
+    if arr.empty?
+      arr << [group[...-1].join.to_i]
+    elsif group.all? { _1 == "\s" }
+      arr << []
+    else
+      arr[-1] << group[...-1].join.to_i
+    end
+  end
+
+  operators = input.lines.map(&:chomp)[-1].split('').reject { _1 == "\s" }
+
+  operations = numbers.zip(operators).map do |(numbers, operator)|
+    if operator == '+'
+      numbers.sum
+    else
+      numbers.reduce(1) do |n, product|
+        product * n
+      end
+    end
+  end
+
+  operations.sum
 end
 
 real_input = File.read('day06-input.txt')
 
+example = File.read('day06-example.txt')
+
 puts "Part 1: #{part1(real_input)}"
 # answer 3261038365331
 puts "Part 2: #{part2(real_input)}"
+# 8342525158298 too low
+# answer 8342588849093
