ed40654926fa6ed28cc099dfd1f1328b7276e579
pierreguillaumelaurin <pierreguillaumelaurin@gmail.com>
2025-12-25 12:23:33 -0500

    Day 7 part 1 refined

--- solutions/day07.rb
+++ solutions/day07.rb
@@ -10,15 +10,14 @@ def part1(input)
 
   count = 0
 
-  (1..grid.length - 1).each_with_object(beam_positions) do |row_index, beam_positions|
-    grid[row_index].each_with_index do |cell, i|
-      has_beam_above = beam_positions[row_index - 1].include?(i)
-      if cell == '^' && has_beam_above
+  (1...grid.length).each do |row_index|
+    beam_positions[row_index - 1].each do |i|
+      if grid[row_index][i] == '^'
         beam_positions[row_index].add(i - 1) if i - 1 >= 0 && i - 1 < grid[row_index].length
         beam_positions[row_index].add(i + 1) if i + 1 >= 0 && i + 1 < grid[row_index].length
         count += 1
-      elsif has_beam_above
-        beam_positions[row_index].add(i) if i >= 0 && i < grid[row_index].length
+      else
+        beam_positions[row_index].add(i)
       end
     end
   end
@@ -33,4 +32,5 @@ end
 real_input = File.read('day07-input.txt')
 
 puts "Part 1: #{part1(real_input)}"
+# answer 1581
 puts "Part 2: #{part2(real_input)}"
