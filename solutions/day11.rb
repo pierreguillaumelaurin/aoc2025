8086a4278d8d6e22340904cf0c193c191398afbd
pierreguillaumelaurin <pierreguillaumelaurin@gmail.com>
2025-12-29 17:28:01 -0500

    Day 11 part 2

--- solutions/day11.rb
+++ solutions/day11.rb
@@ -7,6 +7,20 @@ def count_paths(source, destination, tree)
   tree[source].sum { count_paths(it, destination, tree) }
 end
 
+def count_selected_paths(source, destination, tree, visited = Set.new)
+  return 0 if visited.include?(source)
+
+  path_so_far = visited.union([source])
+
+  if tree[source].include?(destination)
+    return 1 if path_so_far.include?('dac') && path_so_far.include?('fft')
+
+    return 0
+  end
+
+  tree[source].sum { count_selected_paths(it, destination, tree, path_so_far) }
+end
+
 def part1(input)
   tree = input
          .lines
@@ -18,7 +32,13 @@ def part1(input)
 end
 
 def part2(input)
-  # TODO
+  tree = input
+         .lines
+         .map(&:chomp)
+         .map { it.split(': ') }
+         .to_h { |node, neighbors| [node, neighbors.split("\s")] }
+
+  count_selected_paths('svr', 'out', tree)
 end
 
 real_input = File.read('day11-input.txt')
