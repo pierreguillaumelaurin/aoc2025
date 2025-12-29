0f668620f28a5ce1e602c58d7de33f25aac5f491
pierreguillaumelaurin <pierreguillaumelaurin@gmail.com>
2025-12-29 15:38:48 -0500

    Day 10 part 1

---
+++ solutions/day10.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+def part1(input)
+  # TODO
+end
+
+def part2(input)
+  # TODO
+end
+
+real_input = File.read('day10-input.txt')
+
+puts "Part 1: #{part1(real_input)}"
+puts "Part 2: #{part2(real_input)}"
