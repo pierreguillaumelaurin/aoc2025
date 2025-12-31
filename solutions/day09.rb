eea101235bcf63b243b649736ad9d9e6f591147a
pierreguillaumelaurin <pierreguillaumelaurin@gmail.com>
2025-12-30 20:32:44 -0500

    Day 9 part 2

--- solutions/day09.rb
+++ solutions/day09.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'set'
+
 def area((x1, y1), (x2, y2))
   ((x1 - x2).abs + 1) * ((y1 - y2).abs + 1)
 end
@@ -16,8 +18,39 @@ def part1(input)
   distances.max
 end
 
+def add(first_coord, second_coord)
+  [first_coord[0] + second_coord[0], first_coord[1] + second_coord[1]]
+end
+
+def turn_counter_clockwise(direction)
+  [-direction[1], direction[0]]
+end
+
+def trace_borders(corners)
+  starting_point = corners.first
+  direction = [0, 1]
+
+  position = starting_point.dup
+
+  borders = []
+
+  loop do
+    borders << position.dup
+
+    position = add(position, direction)
+
+    break if position == starting_point
+
+    puts borders.inspect
+    direction = turn_counter_clockwise(direction) if corners.include?(position)
+  end
+
+  borders
+end
+
 def part2(input)
-  # TODO
+  red_tiles = input.lines.map(&:chomp).map { _1.split(',').map(&:to_i) }
+  trace_borders(red_tiles)
 end
 
 real_input = File.read('day09-input.txt')
